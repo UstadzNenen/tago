3824e2ed6f2868a868aa96e39980255df65f54fa
Denny Kresno Sunarno <63137098+UstadzNenen@users.noreply.github.com>
2020-04-12 21:55:38 +0700

    Add files via upload

---
+++ config.php
@@ -0,0 +1,9 @@
+<?php
+//UserAgent preferiblemente el suyo
+$useragent="Dalvik/2.1.0 (Linux; U; Android 9; SM-A750GN Build/PPR1.180610.011)";
+//Token
+$token="EAAKBtbLc0qEBAJIt6VWYSBZCUH1wpxf02gEswKsuNTMyuvXH3LO0z0oCz8xAKXKZA39xylx54u9LUhi3TnmZColpWCEDIP4hkqmZAHVTZBtXsTsL7fYcIPXIKHHp4tGMyqR6KLvZCMBpnogLZBXovlatqRA63OPFQsvqxt114zj6V1WLCMWqaWm9vluDg1mXMIE8KXHP4ogteZBcbRinYkpmlwildp87ij8dALCCZYL7OQZDZD
+";
+//ID de Usuario
+$userid="473168";
+?>
